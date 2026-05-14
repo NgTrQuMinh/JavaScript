0ed6e2f9e7dc9a84dde9896841f90ecf4b70c3f7
Ng Tr Quang Minh <nguyentranquangminh207@gmail.com>
2026-05-14 20:17:07 +0700

    Update 01_shopping-list-projectJS

---
+++ .history/001 shopping-list-project-files/shopping-list-start/script_20260514201312.js
@@ -0,0 +1,88 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const itemForm = document.querySelector('#item-form');
+    const itemList = document.querySelector('#item-list');
+    let Datas = [];
+    Datas = JSON.parse(localStorage.getItem('data')) || [];
+
+    const form_Input_filter = document.querySelector('.form-input-filter');
+    let value_form_filter = "";
+
+    function render() {
+        let datas = [...Datas];
+
+        if (value_form_filter !== '') {
+            datas = datas.filter((value) => {
+                return value.text.toLowerCase().trim().includes(value_form_filter.toLowerCase().trim());
+            })
+        }
+
+        itemList.innerHTML = datas.map((value) => {
+            return `
+                <li class="item" data-id="${value.id}">
+                    <span class="item-name ${value.checked ? 'checked' : ''}">${value.text}</span>
+                    <button class="remove-item btn-link text-red">
+                    <i class="fa-solid fa-xmark"></i>
+                    </button>
+                </li>
+            `;
+        }).join('');
+
+        const btnClear = document.querySelector('.btn-clear');
+        btnClear.addEventListener('click', () => {
+            clearLocal();
+            render();
+        })
+    }
+
+    form_Input_filter.addEventListener('input', (e) => {
+        value_form_filter = e.target.value;
+        render();
+    });
+
+    const formInput = document.querySelector('.form-input');
+    itemForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        if (formInput.value === '') return;
+        Datas.push({
+            id: Date.now(),
+            text: formInput.value,
+            checked: false
+        });
+        setLocal();
+        render();
+        itemForm.reset();
+    });
+
+
+
+    itemList.addEventListener('click', (e) => {
+        const item = e.target.closest('.item');
+        if (!item) return;
+        let idItem = item.dataset.id;
+        let indexId = Datas.findIndex((value) => {
+            return value.id === Number(idItem);
+        });
+        if (e.target.closest('.remove-item')) {
+            Datas.splice(indexId, 1);
+        }
+        if (e.target.closest('.item-name')) {
+            Datas[indexId].checked = !Datas[indexId].checked;
+            if (form_Input_filter.value === '') {
+                form_Input_filter.value = Datas[indexId].text;
+            } else {
+                form_Input_filter.value = '';
+            }
+        }
+        setLocal();
+        render();
+    });
+
+
+    function setLocal() {
+        localStorage.setItem('data', JSON.stringify(Datas));
+    }
+    function clearLocal() {
+        localStorage.clear('data', JSON.stringify(Datas));
+    }
+    render();
+});
